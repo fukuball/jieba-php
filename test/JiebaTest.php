3e45faecc1f869258d5c70aafd5a852f6094cc5e
Fukuball Lin <fukuball@gmail.com>
2015-05-01 04:57:56 +0800

    add init test

--- test/JiebaTest.php
+++ test/JiebaTest.php
@@ -7,11 +7,13 @@ class JiebaTest extends PHPUnit_Framework_TestCase
 
     public function testJiebaInit()
     {
+        Jieba::init();
         $this->assertGreaterThan(0, Jieba::$total);
     }
 
     public function testFinalsegInit()
     {
+        Finalseg::init();
         $array_count = count(Finalseg::$prob_start);
         $this->assertEquals(4, $array_count);
     }
