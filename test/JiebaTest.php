6aa2f51d238b2430c748a016508ac54d16ec90f3
Fukuball Lin <fukuball@gmail.com>
2015-05-05 22:57:11 +0800

    補一個 Test 的動作

--- test/JiebaTest.php
+++ test/JiebaTest.php
@@ -32,5 +32,71 @@ class JiebaTest extends PHPUnit_Framework_TestCase
 
         $seg_list = Jieba::cut("怜香惜玉也得要看对象啊！");
         $this->assertEquals($case_array, $seg_list);
+
+        $case_array = array(
+            "我",
+            "来到",
+            "北京",
+            "清华大学"
+        );
+
+        $seg_list = Jieba::cut("我来到北京清华大学");
+        $this->assertEquals($case_array, $seg_list);
+
+        $case_array = array(
+            "他",
+            "来到",
+            "了",
+            "网易",
+            "杭研",
+            "大厦"
+        );
+
+        $seg_list = Jieba::cut("他来到了网易杭研大厦");
+        $this->assertEquals($case_array, $seg_list);
+
+    }
+
+    public function testJiebaCutAll()
+    {
+
+        $case_array = array(
+            "我",
+            "来",
+            "来到",
+            "到",
+            "北",
+            "北京",
+            "京",
+            "清",
+            "清华",
+            "清华大学",
+            "华",
+            "华大",
+            "大",
+            "大学",
+            "学"
+        );
+
+        $seg_list = Jieba::cut("我来到北京清华大学");
+        $this->assertEquals($case_array, $seg_list);
+
+    }
+
+    public function testFinalsegCut()
+    {
+        $case_array = array(
+            "怜香惜",
+            "玉",
+            "也",
+            "得",
+            "要",
+            "看",
+            "对象",
+            "啊"
+        );
+
+        $seg_list = Finalseg::cut("怜香惜玉也得要看对象啊！");
+        $this->assertEquals($case_array, $seg_list);
     }
 }
