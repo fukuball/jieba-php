54c77addc6b13255f2817e19b7858b40c898a594
Fukuball Lin <fukuball@gmail.com>
2015-09-17 16:09:54 +0800

    add codecov support

--- src/cmd/cn_to_zh.php
+++ src/cmd/cn_to_zh.php
@@ -17,7 +17,7 @@ ini_set('memory_limit', '1024M');
 require_once dirname(dirname(__FILE__))."/vendor/zhconverter/Zhconverter.php";
 use Seta0909\Zhconverter;
 
-$content = fopen(dirname(dirname(__FILE__))."/dict/dict.txt", "r");
+$content = fopen(dirname(dirname(__FILE__))."/dict/dict-ex.txt", "r");
 $zh_content = '';
 
 while (($line = fgets($content)) !== false) {
@@ -28,5 +28,5 @@ while (($line = fgets($content)) !== false) {
 
 }
 
-file_put_contents(dirname(dirname(__FILE__))."/dict/dict.zh.txt", $zh_content);
+file_put_contents(dirname(dirname(__FILE__))."/dict/dict-ex.zh.txt", $zh_content);
 ?>
